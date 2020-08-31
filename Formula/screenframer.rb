fcc0c2c813709cb0feb78ab21a2df3b9733c1643
Kacper Raczy <gfw.kra@gmail.com>
2020-08-31 14:48:49 +0200

    Version 1.2.1

--- Formula/screenframer.rb
+++ Formula/screenframer.rb
@@ -1,8 +1,8 @@
 class Screenframer < Formula
   desc "Frame recordings from your apps using device mockups."
   homepage "https://github.com/fredyshox/ScreenFramer"
-  url "https://github.com/fredyshox/ScreenFramer/archive/1.2.0.tar.gz"
-  sha256 "6f47f66e3b0bfb490d2379e0456864b63f717229a191a6398d3b2ddab982a0fe"
+  url "https://github.com/fredyshox/ScreenFramer/archive/1.2.1.tar.gz"
+  sha256 "382043474c29e9d6a4cf42f0ad386061e099210e2c173fa06a8aea3c4dd046da"
   license "MIT"
   head "https://github.com/fredyshox/ScreenFramer.git", :branch => "master"
 
